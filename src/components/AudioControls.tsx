99a082c76f90da6186419180806360e88f4f7069
martis <martynas.grumadas@gmail.com>
2025-03-27 10:55:36 +0200

    tailwindcss added, cleaned up boilerplate

---
+++ src/components/AudioControls.tsx
@@ -0,0 +1,13 @@
+function AudioControls() {
+  return (
+    <>
+      <div className="w-screen bg-stone-200 text-black">
+        <div>Some very amazing One</div>
+        <div>Some very amazing Two</div>
+        <div>Some very amazing Three</div>
+      </div>
+    </>
+  );
+}
+
+export default AudioControls;
