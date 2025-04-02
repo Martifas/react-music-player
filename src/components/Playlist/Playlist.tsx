a561e9f94d2ff0499281f8bb421da48614a16666
martis <martynas.grumadas@gmail.com>
2025-04-02 15:50:07 +0300

    added different playlist viewing

--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -1,16 +1,48 @@
+import { useState } from 'react';
 import { trackList } from '../../lib/tracks';
+import { useFavoriteStore } from '../../states/favoriteState';
 import SongCard from './SongCard';
 
 function Playlist() {
+  const [selectedPlaylist, setSelectedPlaylist] = useState<'all' | 'favorites'>(
+    'all'
+  );
+  const favorites = useFavoriteStore((state) => state.favorites);
+
+  const handlePlaylistChange = (playlist: 'all' | 'favorites') => {
+    setSelectedPlaylist(playlist);
+  };
+
+  const filteredTracks =
+    selectedPlaylist === 'favorites'
+      ? trackList.filter((track) => favorites.has(track.id))
+      : trackList;
+
   return (
     <div className="h-full w-full bg-gradient-to-b from-blue-100 to-blue-200 py-4 px-1">
+      <div className="flex gap-3 py-3">
+        <button
+          className={`rounded-full p-2 ${selectedPlaylist === 'all' ? 'bg-blue-400 hover:bg-blue-300' : 'bg-blue-200 hover:bg-blue-100'}`}
+          onClick={() => handlePlaylistChange('all')}
+        >
+          All Songs
+        </button>
+        <button
+          className={`rounded-full p-2 ${selectedPlaylist === 'favorites' ? 'bg-green-400 hover:bg-green-300' : 'bg-green-200 hover:bg-green-100'}`}
+          onClick={() => handlePlaylistChange('favorites')}
+        >
+          Favorites
+        </button>
+      </div>
+
       <div className="flex font-semibold text-gray-700 border-b border-gray-300 px-1 pb-2 mb-2">
         <span className="flex-1">#</span>
         <span className="flex-6">Title</span>
         <span className="flex-3">Album</span>
         <span className="flex-2 text-right sm:text-left">Duration</span>
       </div>
-      {trackList.map((song, index) => (
+
+      {filteredTracks.map((song, index) => (
         <SongCard
           key={song.id}
           id={song.id}
